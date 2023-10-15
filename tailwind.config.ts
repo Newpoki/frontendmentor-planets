a226873d1a8ec7e896d35d4df3ac82d57db9b7e4
Jason Savelli <jason.savelli@mention.com>
2023-10-15 10:40:17 +0200

    task: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,19 @@ const config: Config = {
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+        colors: {
+            space: 'rgb(7, 7, 36)',
+            'gray-dark': 'rgb(56, 56, 79)',
+            gray: 'rgb(131, 131, 145)',
+            mercury: 'rgb(65, 158, 187)',
+            venus: 'rgb(237, 162, 73)',
+            earth: 'rgb(109, 46, 213)',
+            mars: 'rgb(209, 76, 50)',
+            jupiter: 'rgb(216, 58, 52)',
+            saturn: 'rgb(205, 81, 32)',
+            uranus: 'rgb(30, 193, 162)',
+            neptune: 'rgb(45, 104, 240)',
+        },
         extend: {
             fontFamily: {
                 antonio: ['var(--font-antonio)'],
